7740972effac1a543bc18ed25a86d13446075700
MoXuT0 <DenisFisenko2010@yandex.ru>
2023-01-30 18:28:32 +0300

    Сделано ДЗ

---
+++ src/test/java/UserTest.java
@@ -0,0 +1,42 @@
+import me.denis.User;
+import org.junit.jupiter.api.*;
+
+public class UserTest {
+
+    private String expectedLogin = "test";
+    private String expectedEmail = "test@gmail.com";
+    private User user;
+
+    @BeforeEach
+    public void setUp() {
+        user = new User(expectedLogin, expectedEmail);
+    }
+
+    @Test
+    @DisplayName("Тест на создание объекта User с передачей двух параметров")
+    public void isUserCreated(){
+        Assertions.assertEquals(expectedLogin, user.getLogin(), "Логин не создан");
+        Assertions.assertEquals(expectedEmail, user.getEmail(), "Email не создан");
+    }
+
+    @Test
+    @DisplayName("Тест на создание объекта User без передачи параметров")
+    public void isUserCreatedEmpty() {
+        User userEmpty = new User();
+        Assertions.assertNull(userEmpty.getLogin());
+        Assertions.assertNull(userEmpty.getEmail());
+    }
+
+    @Test
+    @DisplayName("Тест на установленный корректный Email с содержанием @ и знака точки")
+    public void isCorrectEmail() {
+        Assertions.assertTrue((user.getEmail().contains("@")
+                && user.getEmail().contains(".")), "Некорректный email.");
+    }
+
+    @Test
+    public void isLoginEmailNotEquals() {
+        Assertions.assertNotEquals(user.getLogin(), user.getEmail(), "Логин и email совпадают");
+    }
+
+}
